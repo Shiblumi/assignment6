0389338d5d85019450fb73bbc58de0813029d8c7
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-12-05 22:21:17 -0500

    Contact requests being displayed to admin@example.com on contact_requests page.

--- apps/contact_us/controllers.py
+++ apps/contact_us/controllers.py
@@ -6,7 +6,8 @@ from .models import get_user_email
 from py4web.utils.form import Form, FormStyleBulma
 from py4web.utils.grid import Grid, GridClassStyleBulma
 
-# Complete. 
+
+# Index page
 @action('index', method=["GET", "POST"])
 @action.uses('index.html', db, auth.user)
 def index():
@@ -14,3 +15,28 @@ def index():
     if form.accepted:
         redirect(URL('index'))
     return dict(form=form)
+
+# Admin Page
+@action('contact_requests', method=["GET", "POST"])
+@action.uses('contact_requests.html', db, auth.user)
+def contact_requests():
+    user_email = get_user_email()
+    if user_email != 'admin@example.com':
+        redirect(URL('index'))
+        
+    grid = Grid(
+                path=URL('contact_requests'),
+                query=db.contact_requests,
+                orderby=~db.contact_requests.created_on,
+                search_queries=[[
+                    'Search by Name', lambda val: db.contact_requests.name.contains(val),
+                    'By Message', lambda val: db.contact_requests.message.contains(val)
+                ]],
+                editable=False,
+                deletable=True,
+                create=False,
+                details=False,
+                grid_class_style=GridClassStyleBulma,
+                )
+    
+    return dict(grid=grid)
